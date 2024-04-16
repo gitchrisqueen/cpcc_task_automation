a4fdfece3278e7148bdbab1dad15a95c627f6f6d
gitchrisqueen <christopher.queen@gmail.com>
2024-04-16 12:28:51 -0400

    Added initial code to form GUI to grade Exam submissions

--- src/cqc_streamlit_app/pages/4_Grade_Assignment.py
+++ src/cqc_streamlit_app/pages/4_Grade_Assignment.py
@@ -289,7 +289,7 @@ def get_grade_exam_content():
 
                 prompt_value = code_grader.error_definitions_prompt.format_prompt(submission=student_submission_file_path_contents_final)
                 st.header("Prompt Value")
-                st.code(prompt_value)
+                st.code(f"{prompt_value}")
 
                 code_grader.grade_submission(student_submission_file_path_contents)
                 # print("\n\nGrade Feedback:\n%s" % code_grader.get_text_feedback())
