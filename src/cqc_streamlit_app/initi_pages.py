84b28b139a4d91a6b9636b23d9a39598a6af4174
gitchrisqueen <christopher.queen@gmail.com>
2024-04-17 10:48:08 -0400

    Optimize code to allow for UI redraws and faster processing

--- src/cqc_streamlit_app/initi_pages.py
+++ src/cqc_streamlit_app/initi_pages.py
@@ -1,19 +1,18 @@
 #  Copyright (c) 2024. Christopher Queen Consulting LLC (http://www.ChristopherQueenConsulting.com/)
 
 import streamlit as st
-from cqc_cpcc.utilities.env_constants import OPENAI_API_KEY, INSTRUCTOR_USERID, INSTRUCTOR_PASS, FEEDBACK_SIGNATURE
 
 
 # Initialize session state variables
 def init_session_state():
     if 'openai_api_key' not in st.session_state:
-        st.session_state.openai_api_key = OPENAI_API_KEY
+        st.session_state.openai_api_key = ""
 
     if 'instructor_user_id' not in st.session_state:
-        st.session_state.instructor_user_id = INSTRUCTOR_USERID
+        st.session_state.instructor_user_id = ""
 
     if 'instructor_password' not in st.session_state:
-        st.session_state.instructor_password = INSTRUCTOR_PASS
+        st.session_state.instructor_password = ""
 
     if 'instructor_signature' not in st.session_state:
-        st.session_state.instructor_signature = FEEDBACK_SIGNATURE
+        st.session_state.instructor_signature = ""
