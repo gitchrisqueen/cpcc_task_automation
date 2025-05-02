d7cae30e55f58b8b05d069c35b351d0908ecf8bc
gitchrisqueen <christopher.queen@gmail.com>
2025-05-02 08:25:54 -0400

    updated parser to filter using Pydantic 2 schema structure

--- src/cqc_cpcc/utilities/my_pydantic_parser.py
+++ src/cqc_cpcc/utilities/my_pydantic_parser.py
@@ -4,6 +4,7 @@ from typing import List
 
 from langchain_core.output_parsers import PydanticOutputParser
 
+
 class CustomPydanticOutputParser(PydanticOutputParser):
     """Custom parser to modify the schema before getting format instructions."""
 
@@ -13,7 +14,8 @@ class CustomPydanticOutputParser(PydanticOutputParser):
 
     def get_format_instructions(self) -> str:
         # Copy schema to avoid altering original Pydantic schema.
-        schema = {k: v for k, v in self.pydantic_object.schema().items()}
+        # schema = {k: v for k, v in self.pydantic_object.model_json_schema().items()}
+        schema = dict(self.pydantic_object.model_json_schema().items())
 
         # Remove extraneous fields.
         reduced_schema = schema
@@ -22,27 +24,23 @@ class CustomPydanticOutputParser(PydanticOutputParser):
         if "type" in reduced_schema:
             del reduced_schema["type"]
 
-        # Modify the schema to filter errors
-        if "properties" in reduced_schema:
-            if "all_major_errors" in reduced_schema["properties"]:
-                reduced_schema["properties"]["all_major_errors"]["items"]["enum"] = self.major_error_type_list
-            if "all_minor_errors" in reduced_schema["properties"]:
-                reduced_schema["properties"]["all_minor_errors"]["items"]["enum"] = self.minor_error_type_list
-            if "all_feedback" in reduced_schema["properties"]:
-                reduced_schema["properties"]["all_feedback"]["items"]["enum"] = self.feedback_type_list
-
-        if "definitions" in reduced_schema:
-            if "MajorErrorType" in reduced_schema["definitions"]:
-                reduced_schema["definitions"]["MajorErrorType"]["enum"] = self.major_error_type_list
-            if "MinorErrorType" in reduced_schema["definitions"]:
-                reduced_schema["definitions"]["MinorErrorType"]["enum"] = self.minor_error_type_list
-            if "FeedbackType" in reduced_schema["definitions"]:
-                reduced_schema["definitions"]["FeedbackType"]["enum"] = self.feedback_type_list
+        # Modify the schema to filter errors to the ones we want.
+        if "$defs" in reduced_schema:
+            if "MajorErrorType" in reduced_schema["$defs"]:
+                reduced_schema["$defs"].get("MajorErrorType")["enum"] = self.major_error_type_list
+            if "MinorErrorType" in reduced_schema["$defs"]:
+                reduced_schema["$defs"].get("MinorErrorType")["enum"] = self.minor_error_type_list
+            if "FeedbackType" in reduced_schema["$defs"]:
+                reduced_schema["$defs"].get("FeedbackType")["enum"] = self.feedback_type_list
 
         # Ensure json in context is well-formed with double quotes.
-        schema_str = json.dumps(reduced_schema)
+        schema_str = json.dumps(reduced_schema, ensure_ascii=False)
+
+        # Print to the console all the reduced_schema["properties"]
+        print("Reduced Schema Properties: ", schema_str)
 
         return _PYDANTIC_FORMAT_INSTRUCTIONS.format(schema=schema_str)
+
 
 _PYDANTIC_FORMAT_INSTRUCTIONS = """The output should be formatted as a JSON instance that conforms to the JSON schema below.
 
